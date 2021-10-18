ea223e7c86383dac392f29288c6c23a365c415e4
MaxTkachev <max.tkachev@zephyrmobile.com>
2021-10-18 18:35:45 +0300

    ## | Updated Package

--- Package.swift
+++ Package.swift
@@ -7,15 +7,9 @@ let package = Package(
     name: "SlidingRuler",
     platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SlidingRuler",
-            targets: ["SlidingRuler"]),
+        .library(name: "SlidingRuler", targets: ["SlidingRuler"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SlidingRuler")
+        .target(name: "SlidingRuler", path: "Sources")
     ]
 )
